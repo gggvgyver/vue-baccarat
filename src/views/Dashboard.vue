7eb8707efa5bb35673125befc57ed1dd728de5b6
gggvgyver <gggvgyver@gmail.com>
2018-12-17 20:56:07 +0900

    툴팁적용

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,14 +5,20 @@ import func from './vue-temp/vue-editor-bridge';
     <h1 class="subheading grey--text">데시보드</h1>
     <v-container class="my-5">
       <v-layout row class="mb-3">
-        <v-btn small flat color="grey" @click="sortBy('title')">
-          <v-icon left small>folder</v-icon>
-          <span class="caption text-lowercase">By project name</span>
-        </v-btn>
-        <v-btn small flat color="grey" @click="sortBy('person')">
-          <v-icon left small>person</v-icon>
-          <span class="caption text-lowercase">By person</span>
-        </v-btn>
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
+            <v-icon left small>folder</v-icon>
+            <span class="caption text-lowercase">By project name</span>
+          </v-btn>
+          <span>Sort projects by project name</span>
+        </v-tooltip>
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
+            <v-icon left small>person</v-icon>
+            <span class="caption text-lowercase">By person</span>
+          </v-btn>
+          <span>Sort projects by person</span>
+        </v-tooltip>
       </v-layout>
       <v-card flat v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
@@ -61,13 +67,13 @@ export default {
                 },
                 {
                     title: '타이틀3',
-                    person: '사람3',
+                    person: 'a사람3',
                     due: '기간',
                     status: 'ongoing',
                     content: '내용',
                 },
                 {
-                    title: '타이틀4',
+                    title: 'a이틀4',
                     person: '사람4',
                     due: '기간',
                     status: 'overdue',
