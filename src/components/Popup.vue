77a98c63ee7a46487222873764cba5c2c481d864
gggvgyver <gggvgyver@gmail.com>
2018-12-18 20:04:42 +0900

    폼필드

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -5,12 +5,24 @@
       <v-card-title>
         <h2>새프로젝트 추가</h2>
       </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field label="Title" v-model="title"></v-text-field>
+          <v-textarea></v-textarea>
+        </v-form>
+      </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            title: '',
+        }
+    },
+}
 </script>
 
 <style>
